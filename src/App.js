58502f2a98cd5299978f5df5884f2e0bf58f9f60
oladee <deemajor230600@gmail.com>
2023-02-27 15:40:54 +0100

    finished with the instructions for the task

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-function Heading(){
-  
+function Heading() {
+  return <h1>This is an h1 heading </h1>
 }
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return <Heading/>
 }
 
 export default App;
